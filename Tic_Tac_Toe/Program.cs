169a9c52786ca1896e56b9a57797b140ac68c93a
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-10-05 14:06:21 +0530

    Add project files.

---
+++ Tic_Tac_Toe/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe ticTacToe = new TicTacToe();
+            ticTacToe.UseCase1();
+
+        }
+    }
+}
---
+++ Tic_Tac_Toe/TicTacToe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+    class TicTacToe
+    {
+        public void UseCase1()
+        {
+            char[] board = new char[10];
+            for(int i = 1; i<= 9; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
